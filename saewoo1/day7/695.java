00d7b57ffd8ed6b177ee7b11d64d1e63291152b4
Sohyun Park <sohyupar@c4r1s2.42seoul.kr>
2023-04-12 16:24:29 +0900

    695 bfs~~~~

--- saewoo1/day7/695.java
+++ saewoo1/day7/695.java
@@ -20,3 +20,44 @@ class Solution { //또 dfs... bfs로도 풀어볼게여..
         return (1 + getArea(grid, i + 1, j) + getArea(grid, i - 1, j) + getArea(grid, i, j + 1) + getArea(grid, i, j - 1)); // 상하좌우 탐색
     }
 }
+
+//----bfs
+
+class Solution {
+    private int[][]dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int area = 0;
+        int r = grid.length;
+        int c = grid[0].length;
+        int[][] visited = new int[r][c];
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (visited[i][j] == 0 && grid[i][j] > 0) // 방문한 적이 없고, 현 위치가 섬이라면 최대 크기를 찾아 떠난다
+                    area = Math.max(area, getArea(grid, visited, i, j, r, c));
+            }
+        }
+        return area;
+    }
+
+    private int getArea(int[][] grid, int[][] visited, int i, int j, int r, int c) {
+        int area = 0;
+        visited[i][j] = 1; // 방문을 했다고 체크
+        Queue<int[]> res = new LinkedList<>(); // que 생성, int 배열 위치 넣어준다
+        res.add(new int[]{i, j}); // 현 위치를 넣어준다
+        while (!res.isEmpty()) {
+			int[] pos = res.remove(); // pos는 가장 앞의 위치를 받아왔음
+			area++;
+			for (int[] arr : dir) { //4 방향을 순회합니다
+				int x = pos[0] + arr[0];
+				int y = pos[1] + arr[1];
+				if (x >= 0 && x < r && y >= 0 && y < c && grid[x][y] > 0 && visited[x][y] < 1) { // board 내부에 있는 섬이라면
+					res.add(new int[]{x, y}); // 위치를 담아주고, 방문했다고 표기해줌
+					visited[x][y] = 1;
+				}
+			}
+        }
+        return area;
+    }
+}
