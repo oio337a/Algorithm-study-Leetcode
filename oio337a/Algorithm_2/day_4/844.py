61ec9bda402e9f48b6faf0d2f39a416e34c7058f
oio337a <oio337a@gmail.com>
2023-05-05 23:38:17 +0900

    add coments

--- oio337a/Algorithm_2/day_4/844.py
+++ oio337a/Algorithm_2/day_4/844.py
@@ -2,19 +2,19 @@ class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
         s_temp = []
         t_temp = []
-        for letter in s:
-            if letter == '#' and s_temp:
-                s_temp.pop()
+        for letter in s: # s 확인
+            if letter == '#' and s_temp: # '#'이 있고 temp에 값이 있으면
+                s_temp.pop() # temp 의 값 빼고 다음으로
                 continue
-            elif letter != '#':
+            elif letter != '#': # 백스페이스 없으면 값 추가
                 s_temp.append(letter)
-        for letter in t:
+        for letter in t: # t 확인 로직 상동
             if letter == '#' and t_temp:
                 t_temp.pop()
                 continue
             elif letter != '#':
                 t_temp.append(letter)
-        return s_temp == t_temp
+        return s_temp == t_temp # 남은 temp값 비교
 
 
 s = "ab#c"
