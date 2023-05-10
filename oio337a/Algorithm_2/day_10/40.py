eb2df731ae15c8ad6e56f89956bd9517dc924683
Yongmin Park <yongmipa@student.42seoul.kr>
2023-05-10 21:36:42 +0900

    day 10 mission clear:

--- oio337a/Algorithm_2/day_10/40.py
+++ oio337a/Algorithm_2/day_10/40.py
@@ -3,19 +3,43 @@ from itertools import combinations
 
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        def dfs(nums, t, idx, path, res):
-            if t == 0:
-                res.append(path)
-            elif t < 0:
-                return
-            for i in range(idx, len(nums)):
-                if i > idx and nums[i] == nums[i - 1]: # 인덱스가 증가하지 않았거나, nums 의 값이 전값과 같다면 스킵
-                    continue
-            dfs(nums, target - nums[i], i + 1, path + [nums[i]], res)
         res = []
-        dfs(sorted(candidates), target, 0, [], res)
+        self.DFS(sorted(candidates),target,0,[],res)
         return res
+    def DFS(self, nums,target,idx,path,res):
+        if target == 0 : 
+            res.append(path)
+        elif target < 0 :
+            return
+        for i in range(idx,len(nums)):
+            #중복 처리
+            if i > idx and nums[i] == nums[i-1]:
+                continue
+            self.DFS(nums,target-nums[i],i+1,path + [nums[i]],res)
 
+'''
+class Solution:
+    def combinationSum2(self, candidates, target):
+        candidates.sort()                      
+        result = []
+
+        def combine_sum_2(nums, start, path, result, target):
+            if not target:
+                result.append(path)
+                return
+
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i - 1]:
+                    continue
+
+                if nums[i] > target:
+                    break
+
+                combine_sum_2(nums, i + 1, path + [nums[i]], result, target - nums[i])
+
+        combine_sum_2(candidates, 0, [], result, target)
+        return result
+'''
 candidates = [10,1,2,7,6,1,5]
 target = 8
 test = Solution()
