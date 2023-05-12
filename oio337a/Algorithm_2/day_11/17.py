7b43b6d81037473509e4b0cc3bd995da3a73f991
Yongmin Park <yongmipa@student.42seoul.kr>
2023-05-12 16:31:52 +0900

    17 mission clear

--- oio337a/Algorithm_2/day_11/17.py
+++ oio337a/Algorithm_2/day_11/17.py
@@ -2,14 +2,34 @@ from typing import List
 from itertools import combinations
 
 class Solution:
+    # 좀 더 깔끔하게 짤 수 있지만 속도는 비슷하다.
     def letterCombinations(self, digits: str) -> List[str]:
-        letters = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
-        l = len(digits)
-        temp = []
-        for i in digits:
-            temp.append(list(letters[i]))
+        def dfs(index, path):
+            if len(path) == len(digits):
+                result.append(path)
+                return
+            
+            for i in range(index, len(digits)):
+                for j in dic[digits[i]]:
+                    dfs(i + 1, path + j) # i는 조합을 위해 문자가 중복되지 않게 선택할 수 있게 해줌.
+                    # path + j로 넘겼기에 pop이나 join을 해줄 필요가 없다는 장점이있다.
+        if not digits:
+            return []
         
-        return 0
+        dic = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+        
+        result = []
+        dfs(0, "")
+        return result
 
 
 digits = "23"
