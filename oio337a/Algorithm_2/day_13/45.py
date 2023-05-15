b67792d88ba04492db2dce443c315b0e10d2da5b
Yongmin Park <yongmipa@student.42seoul.kr>
2023-05-15 20:02:00 +0900

    45 mission clear

--- oio337a/Algorithm_2/day_13/45.py
+++ oio337a/Algorithm_2/day_13/45.py
@@ -10,9 +10,43 @@ class Solution:
                     continue
                 if counting[i+j] == 0 :
                     counting[i+j] = counting[i]+1
-                    
         return counting[len(nums)-1]
 
+''' 2진탐색 풀이 방법
+import bisect
+from typing import List
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return 0
+
+        if len(nums) == 2:
+            return 1
+
+        steps = [len(nums) - 1]
+
+        for i in range(len(nums) - 2, -1, -1):
+            current_node = nums[i]
+            step = i + current_node
+            idx = bisect.bisect_right(steps, step)
+
+            if idx > 0:
+                steps = steps[idx - 1:]
+                steps.insert(0, i)
+            else:
+                steps.insert(0, step)
+
+        return len(steps) - 1
+
+
+if __name__ == '__main__':
+    print(Solution().jump([1,2,1,1,1]))
+
+'''
 nums = [2,3,0,1,4]
 test = Solution()
 print(test.jump(nums))
