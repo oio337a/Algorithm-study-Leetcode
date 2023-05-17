32170e51677a7b2b7abde76c38f013fd9a14e988
Yongmin Park <yongmipa@student.42seoul.kr>
2023-05-17 15:24:30 +0900

    5 mission clear

---
+++ oio337a/Algorithm_2/day_14/5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        s_len = len(s)
+        if s_len <= 1:
+            return s
+        i, l = 0, 0
+        for j in range(s_len):
+            if s[j - l: j + 1] == s[j - l: j + 1][::-1]:
+                i, l = j - l, l + 1
+            elif j - l > 0 and s[j - l - 1: j + 1] == s[j - l - 1: j + 1][::-1]:
+                i, l = j - l - 1, l + 2
+        return s[i: i + l]
+
+s = "babad"
+print(Solution().longestPalindrome(s))
